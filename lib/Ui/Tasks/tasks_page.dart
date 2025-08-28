ffcf62b89bc7aa1bd754dc7a425445a493d38198
Leen <leenbarazi8@gmail.com>
2025-08-28 14:38:24 +0300

    fix(tasks): use global ITasksRepository provider; remove local repository wiring in TasksPage

--- lib/Ui/Tasks/tasks_page.dart
+++ lib/Ui/Tasks/tasks_page.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../Data/Repositories/tasks_repository.dart';
 import 'tasks_tab_page.dart';
 
 class TasksPage extends StatelessWidget {
   const TasksPage({super.key});
   @override
-  Widget build(BuildContext context) {
-    return RepositoryProvider<ITasksRepository>(
-      create: (ctx) => TasksRepository(ctx.read()),
-      child: const TasksTabPage(),
-    );
-  }
+  Widget build(BuildContext context) => const TasksTabPage();
 }
 
 
